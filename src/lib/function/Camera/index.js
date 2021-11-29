bf1c7d856c141c3741b65009f99193b6b864d78e
eastcopper <ldh7228@gmail.com>
2021-11-30 08:05:03 +0900

    ⚡ :: [Camera] Update Camera

--- src/lib/function/Camera/index.js
+++ src/lib/function/Camera/index.js
@@ -1,1 +1,9 @@
-    
+import * as THREE from "three";
+
+export function SetupCamera(element) {
+  const width = element.clientWidth;
+  const height = element.clientHeight;
+  const camera = new THREE.PerspectiveCamera(75, width / height, 1, 8000); // camera 객체 생성
+  camera.position.set(0,0,2400) 
+  return camera;
+}
