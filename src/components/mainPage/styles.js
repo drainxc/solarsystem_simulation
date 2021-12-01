97e5a0499d6675a956aad777729b60c7f144e923
eastcopper <ldh7228@gmail.com>
2021-12-02 08:26:21 +0900

    ⚡ :: [MainPage] Design MainPage

---
+++ src/components/mainPage/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const MainDiv = styled.div`
+  button {
+    position: absolute;
+    background-color: white;
+    border: none;
+    border-radius: 7px;
+  }
+`;
