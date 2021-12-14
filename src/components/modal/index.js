b98239d11d935e47246f04d4f7a8dfec7fd45dea
eastcopper <ldh7228@gmail.com>
2021-12-14 13:21:45 +0900

    ⚡ :: [ModalWindow] design Modal

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,27 +1,32 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
+import * as S from "./styles";
 
 export default function ModalWindow({ state }) {
+  const modalStyles = {
+    overlay: {
+      backgroundColor: "rgba(255, 255, 255, 0)",
+    },
+    content: {
+      position: "absolute",
+      transition: "0.5s",
+      width: "400px",
+      height: "500px",
+      top: "95px",
+      left: "20px",
+      right: "40px",
+      bottom: "40px",
+    },
+  };
+
   return (
-    <Modal
-      isOpen={state.show}
-      style={{
-        overlay: {
-          backgroundColor: "rgba(255, 255, 255, 0)",
-        },
-        content: {
-          position: "absolute",
-          width: "400px",
-          height: "500px",
-          top: "95px",
-          left: "20px",
-          right: "40px",
-          bottom: "40px",
-        },
-      }}
-    >
-      <div>{state.data.planet}</div>
-      <div>{state.data.explain}</div>
-    </Modal>
+    <>
+      <Modal closeTimeoutMS={500} isOpen={state.show} style={modalStyles}>
+        <S.ContentDiv>
+          <div className="title">{state.data.planet}</div>
+          <div className="contents">{state.data.explain}</div>
+        </S.ContentDiv>
+      </Modal>
+    </>
   );
 }
