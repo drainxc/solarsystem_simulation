b36fca12324cb98c6cf186f11d173d5056953b87
eastcopper <ldh7228@gmail.com>
2021-12-14 15:43:34 +0900

    ⚡ :: [ModalWindow] Design Contents

--- src/components/modal/styles.js
+++ src/components/modal/styles.js
@@ -3,7 +3,11 @@ import styled from "styled-components";
 export const ContentDiv = styled.div`
   .title {
     font-weight: 600;
-    font-size: 30px;
+    font-size: 35px;
     margin-bottom: 15px;
+  }
+  .contents {
+    word-spacing: 3px;
+    font-size: 20px;
   }
 `;
